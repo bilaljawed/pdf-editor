c05b8beb08db891084fe3ce2626dc80ce7452f8f
Bilal Jawed <bilaljawed@users.noreply.github.com>
2026-03-23 13:38:34 +0000

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/pdf-editor/',
   plugins: [react()],
   optimizeDeps: {
     include: ['pdfjs-dist', 'fabric', 'signature_pad', 'pdf-lib'],
